d7688b502434cfc6dfe9f1dcf756b49fb53a6bd3
ku28 <ku28sh07@gmail.com>
2025-03-18 15:02:26 +0530

    add preview components for resume sections

---
+++ components/layout/my-resume/previews/SummaryPreview.tsx
@@ -0,0 +1,28 @@
+import { useFormContext } from "@/lib/context/FormProvider";
+import { themeColors } from "@/lib/utils";
+import React from "react";
+
+const SummaryPreview = () => {
+  const { formData } = useFormContext();
+
+  return (
+    <div>
+      <h2
+        className="text-left font-bold text-sm mb-2"
+        style={{
+          color: formData?.themeColor || themeColors[0],
+        }}
+      >
+        Summary
+      </h2>
+      <hr
+        style={{
+          borderColor: formData?.themeColor || themeColors[0],
+        }}
+      />
+      <p className="text-xs my-2 text-justify">{formData?.summary}</p>
+    </div>
+  );
+};
+
+export default SummaryPreview;
