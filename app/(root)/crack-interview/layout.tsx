518a9417814518f8602b9ad7a555665fb8bd8035
ku28 <ku28sh07@gmail.com>
2025-03-24 15:29:11 +0530

    updates

--- app/(root)/crack-interview/layout.tsx
+++ app/(root)/crack-interview/layout.tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/layout/Header'
 import React from 'react'
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            <Header />
             <div className='mx-5 md:mx-20 lg:mx-36'>
                 {children}
             </div>
